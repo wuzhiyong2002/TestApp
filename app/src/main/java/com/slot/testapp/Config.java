ddca034cb348b3bf5ea9a149b391e71cce0b2270
w <2415183811@qq.com>
2023-10-15 22:05:28 +0800

    delete

--- app/src/main/java/com/slot/testapp/Config.java
+++
@@ -1,8 +0,0 @@
-package com.slot.testapp;
-
-public interface Config {
-    boolean DEBUG = true;
-    String BASEURL = "https://juliang123.top/api/app/init";
-    String APPSFLYER_KEY = "";
-    String ADJUST_KEY = "";
-}
